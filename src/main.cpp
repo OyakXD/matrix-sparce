c683ef91b3e3cd96998a7524c6ce9a327dd215d8
RbmrJnr <ribamarjunior1228@gmail.com>
2025-01-27 15:22:29 -0300

    adicionei a função readSparceMatrix

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <fstream>
 #include "SparseMatrix.h"
 using namespace std;
 
-void readSparseMatrix(SparseMatrix& m, string nome_do_arquivo){
-  // ...
+void readSparseMatrix(SparseMatrix& m, string arquivo){
+  ifstream arquivom(arquivo); // abre e lê o arquivo
+  if(!arquivom.is_open()){ // caso não seja possível abrir o arquivo
+    cout << "Erro ao ler o arquivo"; // saída
+    return;
+  }
+
+  int linhas; 
+  int colunas;
+  arquivom >> linhas >> colunas; // pega o número de linha e colunas da matriz;
+
+
+
+  int lin, col;
+  double valor;
+  while(arquivom >> lin >> col >> valor){ // insere os valores na matriz em seus respectivos locais
+    m.insert(lin, col, valor);
+  }
+
+  arquivom.close(); // fecha o arquivom
+
 }
 
 SparseMatrix sum(SparseMatrix& A, SparseMatrix& B){
@@ -12,4 +32,21 @@ SparseMatrix sum(SparseMatrix& A, SparseMatrix& B){
 
 SparseMatrix multiply (SparseMatrix& A, SparseMatrix& B){
   return A * B;
+}
+
+int main(){
+  // Criar uma matriz esparsa vazia com dimensões genéricas
+    SparseMatrix matrixA(4, 4); // As dimensões serão ajustadas pela leitura do arquivo.
+
+    // Nome do arquivo que será lido
+    string nomeArquivo = "arquivo.txt";
+
+    // Ler a matriz a partir do arquivo
+    readSparseMatrix(matrixA, nomeArquivo);
+
+    // Imprimir a matriz carregada
+    cout << "Matriz carregada do arquivo:" << endl;
+    matrixA.print();
+
+    return 0;
 }
